7414e531c455cd10e2bef661386a0396b048505e
Alfred Neumayer <dev.beidl@gmail.com>
2021-06-28 19:42:41 +0200

    setup: Ask for installing and enabling the OpenSSH server

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,6 +18,50 @@ function warnMissingData {
     source "$CONFIG_ROOT/config.sh"
 }
 
+function tryInstallSshd {
+    if [ "$(uname -s)" == "Linux" ]; then
+        if [ -f /usr/bin/apt ]; then
+            sudo apt install openssh-server && \
+                sudo systemctl enable ssh && \
+                echo "SSH enabled successfully!"
+        else
+            echo "Unknown package manager used, please add support for it in UBports PDK".
+        fi
+    elif [ "$(uname -s)" == "Darwin" ]; then
+        sudo systemsetup -setremotelogin on && echo "SSH enabled successfully!"
+    fi
+}
+
+function checkSsh {
+    if [ "$(uname -s)" == "Linux" ]; then
+        systemctl status ssh 1&> /dev/null
+        if [ "$?" != "0" ]; then
+            echo "WARNING: The OpenSSH server seems to be missing or not activated, please install it using your package manager."
+            while true; do
+                read -p "Would you like to do that automatically now [y/n]? " yn
+                case $yn in
+                    [Yy]* ) tryInstallSshd; break;;
+                    [Nn]* ) break;;
+                    * ) echo "Please answer yes or no.";;
+                esac
+            done
+        fi
+    elif [ "$(uname -s)" == "Darwin" ]; then
+        OUTPUT=$(sudo systemsetup -getremotelogin)
+        if [ "$OUTPUT" != "Remote Login: On" ]; then
+            echo "WARNING: SSH doesn't seem to be enabled!"
+            while true; do
+                read -p "Would you like to enable it now [y/n]? " yn
+                case $yn in
+                    [Yy]* ) tryInstallSshd; break;;
+                    [Nn]* ) break;;
+                    * ) echo "Please answer yes or no.";;
+                esac
+            done
+        fi
+    fi
+}
+
 function setup {
     bash $SCRIPTPATH/scripts/prerequisites.sh
     warnMissingData
@@ -35,6 +79,8 @@ function setup {
         rm "$DATA_ROOT/sshd/id_rsa.pub"
     fi
 
+    checkSsh
+
     ssh-keygen -q -t rsa -N '' -f "$DATA_ROOT/sshd/id_rsa"
     PUBKEY_CONTENTS=$(cat "$DATA_ROOT/sshd/id_rsa.pub")
     if grep -q "^$PUBKEY_CONTENTS" "$HOME/.ssh/authorized_keys"; then
@@ -47,4 +93,5 @@ function setup {
         echo "Inserting ssh key into authorized keys list"
         echo "$PUBKEY_CONTENTS" >> $HOME/.ssh/authorized_keys
     fi
+    echo "Checking for OpenSSH service"
 }
